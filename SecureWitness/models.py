fcb2a337122e0ebfed71c0103419ffbc0b336f8c
JPShrad <jps7ty@virginia.edu>
2015-03-30 13:08:51 -0400

    Report as model

--- SecureWitness/models.py
+++ SecureWitness/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Report(models.Model):
+    author = models.CharField
+    title = models.CharField
+    private = models.BooleanField
+    timestamp = models.DateTimeField.auto_now
+    short_description = models.CharField(max_length=100)
+    long_description = models.CharField(max_length=500)
+    #optional
+    #date of event
+    #location
+    #keywords
+    #files
