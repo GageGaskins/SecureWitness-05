dc6c72d7d83a7b51dae6aca453607363a9776fb4
Rohan <rkd5mb@virginia.edu>
2015-03-30 14:07:26 -0400

    Adding User and Admin classes

---
+++ Admin.py
@@ -0,0 +1,23 @@
+from Report import Report
+
+__author__ = 'Rohan'
+
+
+class Admin:
+    def __init__(self, name):
+        self.name = name
+        self.reports = []
+        self.starred = []
+        self.groups = []
+
+    def starreport(self, report):
+        self.starred.append(report)
+
+    def addreport(self, author, short_description, long_description, title="Title", private=False, date_of_event=None,
+                  location=None, keywords=None, files=None):
+        newreport = Report(author, short_description, long_description, title, private, date_of_event, location,
+                           keywords, files)
+        self.reports.append(newreport)
+
+    def addtogroup(self, user, group):
+        user.groups.append(group)
