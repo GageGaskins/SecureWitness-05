0b6cb36870d3e8ad5b80c394ac52c234a1374aa3
Rohan <rkd5mb@virginia.edu>
2015-04-20 15:53:42 -0400

    Pre Heroku Stuff

--- SecureWitness/admin.py
+++ SecureWitness/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Report, User
+from .models import Report, User, Group, Document
 
 admin.site.register(Report)
 admin.site.register(User)
+admin.site.register(Group)
+admin.site.register(Document)
