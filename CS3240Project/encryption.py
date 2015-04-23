71ef5705eabce3ed023a3893e66c5f729a4e047c
Sri Kodakalla <sk9gn@virginia.edu>
2015-04-23 19:44:25 -0400

    Ideas for encryption setup - not needed

---
+++ CS3240Project/encryption.py
@@ -0,0 +1,14 @@
+__author__ = 'Sri'
+
+
+# For file upload, user can choose to encrypt a file they add
+
+# For file download, check if the files are encrypted and decrypt if necessary
+# Decryption should be done by a stand-alone program
+# Gets the key needed to decrypt the file
+
+# Use public key encryption for encryption and signing
+# Hash functions - verify integrity
+
+# if they have access to the key (access to public key given to them)
+
